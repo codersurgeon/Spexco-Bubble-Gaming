80726c92541e1ee91cf38bf5a1383326e11e5d36
codersurgeon <45948517+codersurgeon@users.noreply.github.com>
2020-03-06 11:32:27 +0300

    review
    code review

--- speXCO/js/app.js
+++ speXCO/js/app.js
@@ -20,48 +20,47 @@ function StartGame(e)
             showPatternModal("Error","Please enter a value.");
              else
                  drawPatternTriangle (areaBlockSize);
-                 clearArea();
 }
 
 function EndGame(e)
 {
     e.preventDefault();
     if(areaBlockSize===null || areaBlockSize===""|| areaBlockSize===undefined )
-         showPatternModal("Error","Enter a value to start the game and press Start Game."); 
-    gameStartInput.value="";    
+         showPatternModal("Error","Enter a value to start the game and press Start Game.");
+    gameStartInput.value="";
     clearArea();
-  
+
 }
 
 function drawPatternTriangle(size)
-{          
+{
     let bubbleArray =[...new Array(parseInt(size))].map((_arr, index) =>
      [...new Array(Math.pow(2,index))].map(() => Math.floor(Math.random() * 100))); // array created
     let sumValue =bubbleArray.map(_array => Math.max(..._array)).reduce((a, b) => a + b); // total value
     for(let i=0; i<size; i++)
-    {           
+    {
         for(let j=0; j<Math.pow(2,i); j++)
         {
             stringHtml+= `<span class='dot'><p>${bubbleArray[i][j]}</p></span>`
-        }        
-        stringHtml+='</br>';  
+        }
+        stringHtml+='</br>';
         root = document.getElementsByTagName('p')[0];
-    } 
-    document.getElementsByClassName("bubbleTriangle")[0].innerHTML=`${stringHtml}` ;     
-    document.getElementsByClassName("maxSumValue")[0].innerHTML=" <strong> Best Total:</strong> "+ `${sumValue}` ;   
-     
+    }
+    document.getElementsByClassName("bubbleTriangle")[0].innerHTML=`${stringHtml}` ;
+    document.getElementsByClassName("maxSumValue")[0].innerHTML=" <strong> Best Total:</strong> "+ `${sumValue}` ;
+
 }
 
 function showPatternModal(modalTitle,modalBody)
 {
-    $("#mdl").trigger('click');  
-    $("#mdl").modal("show");   
+    $("#mdl").trigger('click');
+    $("#mdl").modal("show");
     $(".modalTitle").text(modalTitle);
     $(".modalBody").text(modalBody);
 }
 function clearArea()
-{    
-    document.getElementsByClassName("bubbleTriangle")[0].innerHTML="" ;     
-    document.getElementsByClassName("maxSumValue")[0].innerHTML="";       
+{
+    document.getElementsByClassName("bubbleTriangle")[0].innerHTML="" ;
+    document.getElementsByClassName("maxSumValue")[0].innerHTML="";
 
 }
